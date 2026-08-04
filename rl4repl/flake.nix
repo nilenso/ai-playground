b6013e00710bb099b148b17d08286760fe555162
Ravi Chandra Padmala <neena@nilenso.com>
2026-08-04 23:41:39 +0000

    chore(rl4repl): add just development shell

---
+++ rl4repl/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Development shell with the just command runner";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [ pkgs.just ];
+          };
+        }
+      );
+    };
+}
